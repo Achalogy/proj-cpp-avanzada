d795217819fae90326f7c51b5105d4fcec1814a4
Achalogy <achalogy@gmail.com>
2024-02-23 21:39:44 -0500

    Portada pokemon

--- src/main.cpp
+++ src/main.cpp
@@ -27,5 +27,37 @@ struct SPlayer
 
 int main(void)
 {
+  string *opciones = new string[3];
+
+  *(opciones) = "Crear nueva partida";
+  *(opciones + 1) = "Cargar partida";
+  *(opciones + 2) = "Salir";
+
+  cout << "+-----------------------------------------+" << endl;
+  cout << "|                 POKÉMON                 |" << endl;
+  cout << "|          Edición Programación           |" << endl;
+  cout << "|                                         |" << endl;
+  cout << "|               PRESS START               |" << endl;
+  cout << "|             © 2024 Acha inc             |" << endl;
+  cout << "+-----------------------------------------+" << endl;
+
+  while (true)
+  {
+    int opt = crearMenuApuntadores(3, opciones);
+    switch (opt)
+    {
+    case 1:
+      break;
+    case 2:
+      break;
+    case 3:
+      return 0;
+      break;
+
+    default:
+      break;
+    }
+  }
+
   return 0;
 }
