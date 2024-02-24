16e5739729857ea9abe20c024201097878174bb0
Achalogy <achalogy@gmail.com>
2024-02-23 21:20:56 -0500

    Estructuras

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+struct SPokemon
+{
+  char name[20];
+  short level;
+  short attack;
+  short live;
+  short speed;
+  SPokemon *previous;
+  SPokemon *next;
+  bool mainPlayer;
+  int game;
+};
+
+struct SPlayer
+{
+  char name[20];
+  bool mainPlayer;
+  int game;
+  SPokemon *team;
+};
+
 int main(void)
 {
   return 0;
