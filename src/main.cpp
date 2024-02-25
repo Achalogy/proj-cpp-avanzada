2e3caad3b02a028a3018666158d4faf1119b3595
Achalogy <achalogy@gmail.com>
2024-02-25 16:48:44 -0500

    Mover menu al archivo principal

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <cstring>
-#include "acha/main.h"
 
 using namespace std;
+int crearMenuApuntadores(int cantOpciones, string *opciones);
 
 struct SPokemon
 {
@@ -61,4 +61,26 @@ int main(void)
   }
 
   return 0;
+}
+
+int crearMenuApuntadores(int cantOpciones, string *opciones)
+{
+  // Retorna el entero proporcionado por el usuario que identifica la opción elegida.
+
+  int opcionInput = -1;
+
+  cout << "===========================================" << endl;
+  do
+  {
+    for (int i = 0; i < cantOpciones; i++)
+      cout << i + 1 << ". " << *(opciones + i) << endl;
+    cout << "===========================================" << endl;
+    cout << "> ";
+
+    cin >> opcionInput;
+    cout << endl;
+    // Volver a enviar el menu en caso de no recibir una opción valida34
+  } while (opcionInput <= 0 || opcionInput > cantOpciones);
+
+  return opcionInput;
 }
