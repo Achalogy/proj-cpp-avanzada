5fee31eb364ee8090a12eb48b50f3a6acce5ecea
Achalogy <achalogy@gmail.com>
2024-02-25 17:35:26 -0500

    Crear partida

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 using namespace std;
 int crearMenuApuntadores(int cantOpciones, string *opciones);
+void crearPartida();
+void pause();
+void clearConsole();
 
 struct SPokemon
 {
@@ -25,6 +28,10 @@ struct SPlayer
   SPokemon *team;
 };
 
+SPlayer *jugador = new SPlayer;
+SPlayer *enemigo = new SPlayer;
+int partidasGuardadas = 0;
+
 int main(void)
 {
   string *opciones = new string[3];
@@ -45,9 +52,12 @@ int main(void)
   while (true)
   {
     int opt = crearMenuApuntadores(3, opciones);
+    cin.ignore(); // Ya que se envia un número
+
     switch (opt)
     {
     case 1:
+      crearPartida();
       break;
     case 2:
       break;
@@ -63,6 +73,43 @@ int main(void)
   return 0;
 }
 
+void pause()
+{
+  do
+  {
+    cout << '\n'
+         << "Presiona una tecla para continuar... ";
+  } while (cin.get() != '\n');
+  clearConsole();
+}
+
+void crearPartida()
+{
+  string nombre;
+
+  clearConsole();
+  cout << "¡Te doy la bienvenida al mundo de los Pokémon!" << endl;
+  pause();
+  cout << "Me llamo Acha y soy el Profesor Pokémon de la región de Jave." << endl;
+  pause();
+  cout << "¡Este mundo está lleno de unas criaturitas adorables llamadas Pokémon!" << endl;
+  pause();
+  cout << "Y ahora, háblame un poco de ti. Veamos..." << endl;
+  cout << "¿Cómo te llamas?" << endl
+       << endl
+       << " > ";
+
+  cin >> nombre;
+  strcpy(jugador->name, nombre.c_str());
+  jugador->mainPlayer = true;
+  jugador->team = NULL;
+  jugador->game = partidasGuardadas + 1;
+
+  strcpy(enemigo->name, "Alain");
+  enemigo->team = NULL;
+  enemigo->game = partidasGuardadas + 1;
+}
+
 int crearMenuApuntadores(int cantOpciones, string *opciones)
 {
   // Retorna el entero proporcionado por el usuario que identifica la opción elegida.
@@ -74,8 +121,9 @@ int crearMenuApuntadores(int cantOpciones, string *opciones)
   {
     for (int i = 0; i < cantOpciones; i++)
       cout << i + 1 << ". " << *(opciones + i) << endl;
-    cout << "===========================================" << endl;
-    cout << "> ";
+    cout << "===========================================" << endl
+         << endl;
+    cout << " > ";
 
     cin >> opcionInput;
     cout << endl;
@@ -83,4 +131,15 @@ int crearMenuApuntadores(int cantOpciones, string *opciones)
   } while (opcionInput <= 0 || opcionInput > cantOpciones);
 
   return opcionInput;
+}
+
+void clearConsole()
+{
+  // Limpia la consola, usa un if para saber si es linux o windows
+
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
 }
